47e1834176bdf9784846b19af5253330db130a6c
Aman Jaiswal <87319499+amanjaiswal0777@users.noreply.github.com>
2023-08-07 12:11:21 +0530

    Add files via upload

---
+++ jv.js
@@ -0,0 +1,34 @@
+var navMenuAnchorTags = document.querySelectorAll('.new a');
+console.log(navMenuAnchorTags);
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        // console.log(targetSectionID);
+        var targetSection=document.getElementById(targetSectionID);
+        // console.log(targetSection);
+
+        //    interval = setInterval(scrollVertically, 20, targetSection);
+
+        var interval = setInterval(function () {
+            // var targetSectionCoordinates=targetSection.getBoundingClientRect;
+            // if(targetSectionCoordinates.top<=0){
+            //     clearInterval(interval);
+            //     return;
+            // }
+            // window.scrollBy(0,50);
+            scrollVertically(targetSection);
+        },20);
+       
+    });
+}
+
+
+function scrollVertically(targetSection) {
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollTo(0, 50);
+}
